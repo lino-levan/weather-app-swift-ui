24bedc422dd897226b89a7cbbe16123794af11fe
lino-levan <11367844+lino-levan@users.noreply.github.com>
2022-04-13 08:09:24 -0700

    Initial Commit

---
+++ pintform/pintformApp.swift
@@ -0,0 +1,17 @@
+//
+//  pintformApp.swift
+//  pintform
+//
+//  Created by Lino Le Van on 4/13/22.
+//
+
+import SwiftUI
+
+@main
+struct pintformApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
