c83c49b6f3d1d4ef93dd622427771ff15b82c34e
lino-levan <11367844+lino-levan@users.noreply.github.com>
2022-04-29 10:37:44 -0700

    Created the example app

---
+++ weather/WeatherButton.swift
@@ -0,0 +1,24 @@
+//
+//  WeatherButton.swift
+//  pintform
+//
+//  Created by Lino Le Van on 4/13/22.
+//
+
+import SwiftUI
+
+struct WeatherButton: View {
+    
+    var title: String
+    var textColor: Color
+    var backgroundColor: Color
+    
+    var body: some View {
+        Text(title)
+            .frame(width: 280, height: 50)
+            .foregroundColor(textColor)
+            .background(backgroundColor)
+            .font(.system(size: 20, weight: .bold, design: .default))
+            .cornerRadius(10.0)
+    }
+}
